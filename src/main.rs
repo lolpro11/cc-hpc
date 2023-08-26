3423aa028b29f5cb121dce30908d8984367559e9
lolpro11 <lolpro11lolx@gmail.com>
2023-08-26 14:50:01 -0700

    works with biguint

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-fn main() {
-    for i in (u128::MIN + 1)..u128::MAX {
-        let mut num = i;
-        while num != 1 {
-            if (num % 2) == 1 {
-                num = ((3 * num) + 1)/2
+use std::{fs::File, io::{Read, self}};
+use num_bigint::{BigUint, ToBigUint};
+use num_traits::{One, Euclid};
+
+fn main() -> io::Result<()> {
+    let mut file = File::open("verifed_number.txt")?;
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer)?;
+
+    let number: BigUint = buffer.trim().parse().expect("Failed to parse number");
+
+    println!("Number read from file: {}", number);
+    let mut num: BigUint = number;
+    let mut temp_num = num.clone();
+    loop {
+        while temp_num != 1.to_biguint().unwrap() {
+            if temp_num.rem_euclid(&2.to_biguint().unwrap()) == 1.to_biguint().unwrap() {
+                temp_num = ((3.to_biguint().unwrap() * temp_num) + 1.to_biguint().unwrap()) / 2.to_biguint().unwrap();
+                //println!("{}", temp_num);
             } else {
-                num = num / 2
+                temp_num = temp_num / 2.to_biguint().unwrap();
+                //println!("{}", temp_num);
             }
         }
-        println!("{} leads to 1 via CC" , i);
+        println!("{} leads to 1 via CC" , num.clone());
+        num = num + 1.to_biguint().unwrap();
     }
-
+    Ok(())
 }
