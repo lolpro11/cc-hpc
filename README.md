33dddb877e7eee2c9c74126ce6f143350db8ec66
lolpro11 <lolpro11lolx@gmail.com>
2023-08-27 06:40:37 -0700

    added stuff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cc-hpc
 Collatz conjecture HPC
+Verifed Number from
+https://youtu.be/094y1Z2wpJg?t=648
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use num_bigint::{BigUint, ToBigUint};
 use num_traits::Euclid;
 
 fn main() -> io::Result<()> {
-    let mut file = File::open("verifed_number.txt")?;
+    let mut file = File::open("number.txt")?;
     let mut buffer = String::new();
     file.read_to_string(&mut buffer)?;
 
@@ -14,19 +14,19 @@ fn main() -> io::Result<()> {
     let mut temp_num = num.clone();
     loop{
         temp_num = num.clone();
+        print!("\n{}", temp_num);
         while temp_num != 1.to_biguint().unwrap() {
             if temp_num.rem_euclid(&2.to_biguint().unwrap()) == 1.to_biguint().unwrap() {
                 temp_num = ((3.to_biguint().unwrap() * temp_num) + 1.to_biguint().unwrap()) / 2.to_biguint().unwrap();
-                println!("{}", temp_num);
+                print!(" {}", temp_num);
             } else {
                 temp_num = temp_num / 2.to_biguint().unwrap();
-                println!("{}", temp_num);
+                print!(" {}", temp_num);
             }
             if temp_num <= num {
                 break;
             }
         }
-        println!("{} leads to 1 via CC" , num.clone());
         num = num + 1.to_biguint().unwrap();
     }
     Ok(())
