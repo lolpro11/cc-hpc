3f264a1baf7657d106966f8050328161c618f554
lolpro11 <lolpro11lolx@gmail.com>
2023-08-26 18:41:02 -0700

    added pre number verify

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::{Read, self}};
 use num_bigint::{BigUint, ToBigUint};
-use num_traits::{One, Euclid};
+use num_traits::Euclid;
 
 fn main() -> io::Result<()> {
     let mut file = File::open("verifed_number.txt")?;
@@ -12,14 +12,18 @@ fn main() -> io::Result<()> {
     println!("Number read from file: {}", number);
     let mut num: BigUint = number;
     let mut temp_num = num.clone();
-    loop {
+    loop{
+        temp_num = num.clone();
         while temp_num != 1.to_biguint().unwrap() {
             if temp_num.rem_euclid(&2.to_biguint().unwrap()) == 1.to_biguint().unwrap() {
                 temp_num = ((3.to_biguint().unwrap() * temp_num) + 1.to_biguint().unwrap()) / 2.to_biguint().unwrap();
-                //println!("{}", temp_num);
+                println!("{}", temp_num);
             } else {
                 temp_num = temp_num / 2.to_biguint().unwrap();
-                //println!("{}", temp_num);
+                println!("{}", temp_num);
+            }
+            if temp_num <= num {
+                break;
             }
         }
         println!("{} leads to 1 via CC" , num.clone());
